c720a9cf8ce17f73e76d6fef231c3d0c953e4ad5
Mario Ramalho <mario@ramalho.ch>
2024-04-16 23:39:24 +0200

    Commit JavaDocApp

---
+++ src/javadoc/Application.java
@@ -0,0 +1,18 @@
+package javadoc;
+
+/**
+ * Application de base pour élaborer la javadoc de méthode
+ * @author ramalhom
+ */
+public class Application {
+
+    /**
+     * Démarrage de l'application
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
